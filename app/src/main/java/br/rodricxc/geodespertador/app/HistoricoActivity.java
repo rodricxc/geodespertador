48adfbb99a23e12d9a2dedb0678bbdab4d4ca81b
Rodrigo Chaves <rodrichaves2010@gmail.com>
2014-06-04 17:59:44 -0300

    chamando a acativity do histórico pela activity principal

--- app/src/main/java/br/rodricxc/geodespertador/app/HistoricoActivity.java
+++ app/src/main/java/br/rodricxc/geodespertador/app/HistoricoActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class HistoricoActivity extends Activity {
 
@@ -11,5 +13,13 @@ public class HistoricoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historico);
+
+        final Button buttonVoltar = (Button) findViewById(R.id.button_sair);
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/br/rodricxc/geodespertador/app/MainActivity.java
+++ app/src/main/java/br/rodricxc/geodespertador/app/MainActivity.java
@@ -1,10 +1,12 @@
 package br.rodricxc.geodespertador.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +14,25 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonHistorico = (Button) findViewById(R.id.button_historico);
+
+
+
+        buttonHistorico.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                goHistorico();
+            }
+        });
+
+
+    }
+
+    public void goHistorico() {
+        Intent i  = new Intent(this, HistoricoActivity.class);
+        startActivity(new Intent(this, HistoricoActivity.class));
+        return;
     }
 
 
